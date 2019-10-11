1133694d1132d09f866d21c01e90ccb17526969d
Shashwat Srivastava <shashwatsr2712@gmail.com>
2019-10-11 15:51:00 +0530

    add files

---
+++ app.js
@@ -0,0 +1,58 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+//const User=require("./models/user")
+
+//Count of Users and IP address of socket
+let Users=0,ip;
+
+app.use(express.static(__dirname+"/public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",(req,res) => {
+    ip = req.headers['X-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    res.render("index",{Users:Users});
+});
+
+
+//Server listening (Port config)
+const port=process.env.PORT || 3000;
+const server=app.listen(port,() => {
+    console.log('Server has been started!');
+});
+
+let io=require("socket.io")(server);
+
+io.on("connection",(socket) => {
+    Users+=1;
+    console.log("New User connected!");
+    socket.username='Anonymous('+ip+')';
+    //Username in socket stores IP address as well
+    socket.on('changeUserName',(data) => {
+        socket.username=data.username+'('+ip+')';
+        socket.emit("changeUserName",{username:data.username});
+    });
+    socket.on("newMessage",(data) => {
+        //Stripping IP address from socket's username to get plain username
+        let startIP=socket.username.lastIndexOf('(');
+        let endIP=socket.username.lastIndexOf(')');
+        let usernameWithoutIP=socket.username.substring(0,startIP);
+        let IPOfSender=socket.username.substring(startIP+1,endIP);
+        //Check whether client is the one who sent the message
+        let ISentTheMessage=false;
+        if(ip==IPOfSender){
+            ISentTheMessage=true;
+        }
+        // console.log("Hey "+ip+" "+IPOfSender+" "+socket.username+" "+startIP+" "+endIP);
+        //broadcast
+        io.sockets.emit("newMessage",{message:data.message,username:usernameWithoutIP,sender:ISentTheMessage});
+    });
+    socket.on("disconnect",function(){
+        Users-=1;
+        console.log('A user disconnected!');
+    });
+});
---
+++ public/scripts/chat.js
@@ -0,0 +1,48 @@
+$(function(){
+    //Connection
+    let socket=io.connect();
+
+    //Variables declaration
+    let username=$("#userName");
+    let setUserName=$("#setName");
+    let sendMessage=$("#sendMessage");
+    let message=$("#messageToSend");
+    let chatSpace=$("#chatSpace");
+    let helloAlert=$("#helloUser");
+
+    //Emit an event for setting name
+    setUserName.click(function(){
+        socket.emit("changeUserName",{username:username.val()});
+    });
+
+    //Listening for change of Username
+    socket.on("changeUserName",(data) => {
+        helloAlert.empty();
+        helloAlert.append("Welcome "+data.username+"!");
+        helloAlert.fadeIn();
+        helloAlert.fadeOut(2000);
+    });
+
+    //Emit event for sending message
+    sendMessage.click(function(){
+        socket.emit("newMessage",{message:message.val()});
+    });
+
+    //Listening for new messages
+    socket.on("newMessage",(data) => {
+        message.val('');
+        //Check if current client is sender or receiver
+        let marginSet="margin-right:auto;border-top-right-radius:10px;border-bottom-right-radius:10px;";
+        if(data.sender){
+            marginSet="margin-left:auto;border-top-left-radius:10px;border-bottom-left-radius:10px;";
+        }
+        //Choosing a random bg color for chatbox
+        let back = ["#D3FEFC","#CCFCD1","#F0F5BD","#FAD48C","#D2FCFC","#D2ECFC","#EEDDFE","#FDE4FD","#FCE0DA"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        let styling="style='"+marginSet+"background-color:"+rand+";max-width:80vw;padding:2vh 2vw 2vh 2vw;'";
+        chatSpace.append("<div class='message' "+styling+"><strong>"+data.username+"</strong><br/>"+data.message+"</div><br/>");
+        //Scroll Chat Space to top when new message comes
+        chatSpace.animate({scrollTop:$(document).height()},'slow');
+    });
+
+});
